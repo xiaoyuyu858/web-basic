09e0a96fe2cdfa0c8486540077b52e580bed17a5
Quynh Money <xiaoyuyu858@gmail.com>
2022-03-19 14:52:29 +0700

    vuighe.net

---
+++ day5/script.js
@@ -0,0 +1,30 @@
+let MyName = "SuSu";
+let MyAge = 30;
+let message =
+  "hello" +
+  " " +
+  "I'm" +
+  " " +
+  MyName +
+  " " +
+  "I'm" +
+  " " +
+  MyAge +
+  " " +
+  "yearsold";
+console.log(message);
+
+function address(address) {
+  console.log("this is my", address);
+  console.log("Boston City");
+  console.log("Massachuselts State");
+  console.log("United States of America Country");
+}
+address("address");
+function home(address) {
+  console.log("My address is", address);
+  console.log("Ha Noi city");
+  console.log("Viet Nam Country");
+}
+home("123 Thai Ha Street");
+
